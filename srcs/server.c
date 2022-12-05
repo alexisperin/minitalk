e3d5433b3740ece535b21c7bc4274c4e9727ae5d
aperin <aperin@student.s19.be>
2022-12-05 14:40:13 +0100

    New server decoder

--- srcs/server.c
+++ srcs/server.c
@@ -6,7 +6,7 @@
 /*   By: aperin <aperin@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 17:41:56 by aperin            #+#    #+#             */
-/*   Updated: 2022/12/05 13:32:43 by aperin           ###   ########.fr       */
+/*   Updated: 2022/12/05 14:39:36 by aperin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,38 +16,20 @@
 #include "minitalk.h"
 #include "ft_printf.h"
 
-void	binary_to_char(int binary[CHAR_BIT])
-{
-	char	c;
-	int		i;
-
-	c = 0;
-	i = 0;
-	while (i < CHAR_BIT)
-	{
-		c *= 2;
-		c += binary[i];
-		i++;
-	}
-	write(1, &c, 1);
-}
-
 void	signal_handler(int signum)
 {
-	static int	binary[CHAR_BIT];
-	static int	i;
+	static char	c = 0;
+	static int	bits_received = 0;
 
-	if (signum == SIGUSR1)
-		binary[i] = 0;
-	else
-		binary[i] = 1;
-	if (i < CHAR_BIT - 1)
-		i++;
-	else
+	c += (signum == SIGUSR2);
+	bits_received++;
+	if (bits_received == CHAR_BIT)
 	{
-		i = 0;
-		binary_to_char(binary);
+		write(1, &c, 1);
+		bits_received = 0;
+		c = 0;
 	}
+	c = c << 1;
 }
 
 int	main(void)
@@ -55,11 +37,12 @@ int	main(void)
 	pid_t	pid;
 
 	pid = getpid();
-	ft_printf("%d\n", pid);
+	ft_printf("Server PID: %d\n", pid);
 	signal(SIGUSR1, signal_handler);
 	signal(SIGUSR2, signal_handler);
 	while (1)
 	{
 		pause();
 	}
+	return (0);
 }
